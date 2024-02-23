02a44e94ecf44cb1f0ee3895a7afd24d27578747
vitor <hugov3141v@gmail.com>
2024-02-23 16:13:17 -0300

    .

---
+++ models.js
@@ -0,0 +1,75 @@
+let modelJson = [
+// Objeto 1
+{
+    Id: 1,
+    name: 'F4F wildcat',
+    img: ' img/F4Wildcat.png ',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'Caça utilizado nos primeiros anos de combate no pacifico pela marinha dos EUA'
+},
+
+// Objeto 2
+{
+    Id: 2,
+    name: 'Focke wulf Fw190 A5',
+    img: ' img/Fw190A5.png ',
+    price: [80.00, 150.00, 200.00],
+    sizes: ['1/144', '1/48', '1/32'],
+    description: 'Caça de motor em estrela da Luftaffe, temido em médias e baixas altitudes'
+},
+// Objeto 3
+{
+    Id: 3,
+    name: 'Ki 61 - Hien',
+    img: ' img/Fi61Hien.png ',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'Caça fabricado pela Kawasaki para exercito japonês, com motor em linha'
+},
+// Objeto 4
+{
+    Id: 4,
+    name: 'Me 109 G6',
+    img: ' img/Me109G6.png ',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'Famoso caça da Messerschimitt, voado pelos maiores ases da Luftwaffe, a versão G foi a mais produzida'
+},
+// Objeto 5
+{
+    id: 5,
+    name: 'P47 D - Thunderbolt',
+    img: 'img/P47D.png',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'Caça bombardeiro de fabricação americana utilizado pela FAB'
+},
+// Objeto 6
+{
+    id: 6,
+    name: 'P51 D - Mustang',
+    img: 'img/P51D.png',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'O mais conhecido caça americano, importante escolta para os bomardeiros diurnos'
+},
+// Objeto 7
+{
+    id: 7,
+    name: 'Spitfire mk-IX',
+    img: 'img/Spitmk9.png',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'O mais famoso caça britanico, utilizado em toda a guerra'
+},
+// Objeto 8
+{
+    id: 8,
+    name: 'A6M Zero',
+    img: 'img/Zero.png',
+    price: [100.00, 150.00, 200.00],
+    sizes: ['1/72', '1/48', '1/32'],
+    description: 'O mais famoso caça da marinha japonesa, terror do pacífico nos anos iniciais da guerra'
+},
+];
